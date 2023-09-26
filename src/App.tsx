323fd0d1ffe86ac5e8fdcd61b04eed54b5ca43f8
Seyyednavid Hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-09-26 22:25:15 +0100

    typescript in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import ReminderList from "./components/ReminderList";
+import Reminder from "./models/Reminder";
+import ReminderService from "./services/ReminderService";
+import NewReminder from "./components/NewReminder";
 
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+
+  useEffect(() => {
+    loadReminders();
+  }, []);
+
+  const loadReminders = async () => {
+    const reminders = await ReminderService.getReminders();
+    setReminders(reminders);
+  };
+
+  const removeReminder = (id: number) => {
+    setReminders(reminders.filter((item) => item.id !== id));
+  };
+
+  const addReminder = async (title: string) => {
+    const newReminder = await ReminderService.addReminders(title);
+    setReminders((prevReminders) => [newReminder, ...prevReminders]);
+  };
+  console.log(reminders);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <NewReminder onAddReminder={addReminder} />
+      <ReminderList items={reminders} onRemoveReminder={removeReminder} />
     </div>
   );
 }
